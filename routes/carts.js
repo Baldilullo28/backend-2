b9952164f35387490a4b7b5e2a4fb4782223927e
Baldilullo28 <balerydilullo8gmail.com>
2023-10-31 22:52:19 -0300

    se realizaron cambios en el proyecto

--- routes/carts.js
+++ routes/carts.js
@@ -2,6 +2,54 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 
-// Defino las rutas y funciones de manejo de carritos aquí
+
+router.use((req, res, next) => {
+    try {
+        
+        const cartsData = fs.readFileSync('data/carrito.json', 'utf-8');
+        carts = JSON.parse(cartsData);
+    } catch (error) {
+        carts = [];
+    }
+    next();
+});
+
+router.post('/', (req, res) => {
+    const newCart = req.body;
+    newCart.id = generateCartId(); 
+    carts.push(newCart);
+    persistCarts(); 
+    res.status(201).json(newCart);
+});
+
+router.get('/:cid', (req, res) => {
+    const { cid } = req.params;
+    const cart = carts.find((c) => c.id === cid);
+    if (cart) {
+        res.json(cart.products);
+    } else {
+        res.status(404).json({ error: 'Carrito no encontrado' });
+    }
+});
+
+router.post('/:cid/product/:pid', (req, res) => {
+    const { cid, pid } = req.params;
+    const { quantity } = req.body;
+    const cart = carts.find((c) => c.id === cid);
+    const product = products.find((p) => p.id === pid); 
+
+    if (cart && product) {
+        const existingProduct = cart.products.find((p) => p.product === pid);
+        if (existingProduct) {
+            existingProduct.quantity += quantity;
+        } else {
+            cart.products.push({ product: pid, quantity });
+        }
+        persistCarts(); 
+        res.json(cart.products);
+    } else {
+        res.status(404).json({ error: 'Carrito o producto no encontrado' });
+    }
+});
 
 module.exports = router;
--- routes/products.js
+++ routes/products.js
@@ -2,6 +2,65 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 
-// Defino las rutas y funciones de manejo de productos aquí
+
+router.use((req, res, next) => {
+    try {
+       
+        const productsData = fs.readFileSync('data/productos.json', 'utf-8');
+        products = JSON.parse(productsData);
+    } catch (error) {
+        products = [];
+    }
+    next();
+});
+
+router.get('/', (req, res) => {
+    res.json(products);
+});
+
+
+router.get('/:pid', (req, res) => {
+    const { pid } = req.params;
+    const product = products.find((p) => p.id === pid);
+    if (product) {
+        res.json(product);
+    } else {
+        res.status(404).json({ error: 'Producto no encontrado' });
+    }
+});
+
+
+router.post('/', (req, res) => {
+    const newProduct = req.body;
+    newProduct.id = generateProductId(); 
+    products.push(newProduct);
+    persistProducts(); 
+    res.status(201).json(newProduct);
+});
+
+router.put('/:pid', (req, res) => {
+    const { pid } = req.params;
+    const updatedProduct = req.body;
+    const index = products.findIndex((p) => p.id === pid);
+    if (index !== -1) {
+        products[index] = { ...products[index], ...updatedProduct };
+        persistProducts(); 
+        res.json(products[index]);
+    } else {
+        res.status(404).json({ error: 'Producto no encontrado' });
+    }
+});
+
+router.delete('/:pid', (req, res) => {
+    const { pid } = req.params;
+    const index = products.findIndex((p) => p.id === pid);
+    if (index !== -1) {
+        const deletedProduct = products.splice(index, 1);
+        persistProducts(); 
+        res.json(deletedProduct[0]);
+    } else {
+        res.status(404).json({ error: 'Producto no encontrado' });
+    }
+});
 
 module.exports = router;
